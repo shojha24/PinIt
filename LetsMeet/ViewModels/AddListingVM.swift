65bfc3da65353702b3b89ba34d80c71f6733ec23
shojha24 <79208016+shojha24@users.noreply.github.com>
2025-01-24 20:12:38 -0800

    All item fields added except for location; sheet binding yet to be done

--- LetsMeet/ViewModels/AddListingVM.swift
+++ LetsMeet/ViewModels/AddListingVM.swift
@@ -6,18 +6,62 @@
 //
 
 import Foundation
+import MapKit
 
 class AddListingVM : ObservableObject {
-    @Published var title : String = ""
-    @Published var date : Date = Date()
-    @Published var description : String = ""
-    @Published var location : String = ""
-    @Published var profile : String = ""
-    @Published var visible : Bool = true
-    @Published var capacity : Int = 0
+    @Published var title : String = "" // done
+    @Published var date : Date = Date() // done
+    @Published var description : String = "" // done
+    @Published var location : CLLocationCoordinate2D = CLLocationCoordinate2D()
+    @Published var profile : String = "" // done
+    @Published var visible : Bool = true // done
+    @Published var capacity : String = ""
     @Published var unlimitedCap : Bool = true
     
     init () {
         
+    }
+}
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+   
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+        print(#function, statusString)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        print(#function, location)
     }
 }
--- LetsMeet/Views/AddListingView.swift
+++ LetsMeet/Views/AddListingView.swift
@@ -7,35 +7,51 @@
 
 import SwiftUI
 import MapKit
-import LocationPicker
-
+import MapItemPicker
 
 struct AddListingView: View {
     @StateObject var viewModel = AddListingVM()
+    @StateObject var locationModel = LocationManager()
     
     init(userId: String) {
         viewModel.profile = userId
+        viewModel.location = locationModel.lastLocation?.coordinate ?? CLLocationCoordinate2D(latitude: 37.333747, longitude: -122.011448)
     }
     
     var body: some View {
         VStack {
-            Text("New Listing")
-                .font(.system(size: 32))
-                .bold()
-            
-            Form {
-                // Title
-                TextField("Title", text: $viewModel.title)
-                    .textFieldStyle(DefaultTextFieldStyle())
+                Text("New Listing")
+                    .font(.system(size: 32))
+                    .bold()
                 
-                TextField("Description", text: $viewModel.description, axis: .vertical)
-                    .textFieldStyle(DefaultTextFieldStyle())
-                                
-                DatePicker("Due Date", selection: $viewModel.date)
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                
-                
-            }
+                Form {
+                    // Title
+                    TextField("Title", text: $viewModel.title)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    TextField("Description", text: $viewModel.description, axis: .vertical)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Picker("Visibility", selection: $viewModel.visible) {
+                        Text("Public").tag(true)
+                        Text("Private").tag(false)
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    if (viewModel.visible) {
+                        Toggle("Unlimited spots?", isOn: $viewModel.unlimitedCap)
+                            .toggleStyle(.switch)
+                    }
+                    
+                    if (!viewModel.unlimitedCap) {
+                        TextField("Number of spots available", text: $viewModel.capacity)
+                                    .keyboardType(.decimalPad)
+                    }
+                    
+                    DatePicker("Due Date", selection: $viewModel.date)
+                        .datePickerStyle(GraphicalDatePickerStyle())
+                    
+                }
         }
     }
 }
